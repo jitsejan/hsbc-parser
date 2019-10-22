b5e1403112f9156fb60e19324c261054e239f312
Jitse-Jan <code@jitsejan.com>
2019-10-22 15:19:51 +0100

    Feed statement through argument to console program

--- __main__.py
+++ __main__.py
@@ -1,6 +1,30 @@
 from hsbcpdfreader import HSBCPdfReader
 from hsbccreditcardpdfreader import HSBCCreditCardPdfReader
 
-hpr = HSBCCreditCardPdfReader('/Users/j.waterschoot/Downloads/Statement-3.pdf')
-df = hpr.get_dataframe()
-print(df)
+import argparse
+import os
+
+def _get_args():
+    """ Get the arguments """
+    parser = argparse.ArgumentParser(
+        description="HSBC statement parser"
+    )
+    parser.add_argument(
+        "--statement",
+        nargs="?",
+        help="Data source",
+        required=True,
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = _get_args()
+    if os.path.isfile(args.statement):
+        hpr = HSBCCreditCardPdfReader(args.statement)
+        df = hpr.get_dataframe()  
+        print(df)
+    
+
+if __name__ == "__main__":
+    main()
